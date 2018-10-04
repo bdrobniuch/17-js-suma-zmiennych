ed2dbf513366aadf1724a513e647b3a943063fae
bdrobniuch <drobniuch@gmail.com>
2018-10-04 19:02:56 +0200

    dodawanie

---
+++ js/main.js
@@ -0,0 +1,9 @@
+function dodaj(l1, l2) {
+    var suma = l1 + l2;
+    return suma;              
+}
+
+var liczbaPierwsza = 9;
+var liczbaDruga = 13;
+
+console.log(dodaj(liczbaPierwsza, liczbaDruga));
